6cafe538658cd7ac12ccfb87df2c00d0c55493af
VeerHan <leoleohan@foxmail.com>
2019-03-17 16:26:50 +0100

    Fix NPE when title, description or url is null when fetched from web-service

--- app/src/main/java/com/akshay/newsapp/model/NewsArticles.kt
+++ app/src/main/java/com/akshay/newsapp/model/NewsArticles.kt
@@ -15,8 +15,8 @@ import com.google.gson.annotations.SerializedName
 data class NewsArticles(
         @PrimaryKey(autoGenerate = true) var id: Int = 0,
         @SerializedName("author") var author: String? = null,
-        @SerializedName("title") var title: String = "",
-        @SerializedName("description") var description: String = "",
-        @SerializedName("url") var url: String = "",
+        @SerializedName("title") var title: String? = null,
+        @SerializedName("description") var description: String? = null,
+        @SerializedName("url") var url: String? = null,
         @SerializedName("urlToImage") var urlToImage: String? = null,
         @SerializedName("publishedAt") var publishedAt: String? = null)
