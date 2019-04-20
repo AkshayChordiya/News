8d098072729baac7b31b581f4d4eb6cf805336c5
AkshayChordiya <akshaychordiya2@gmail.com>
2019-04-20 10:00:31 +0200

    Convert `AppExecutors` to Kotlin and add to DI graph

---
+++ app/src/main/java/com/akshay/newsapp/AppExecutors.kt
@@ -0,0 +1,42 @@
+package com.akshay.newsapp
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Global executor pools for the whole application.
+ *
+ * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ */
+@Singleton
+open class AppExecutors(
+        private val diskIO: Executor,
+        private val networkIO: Executor,
+        private val mainThread: Executor
+) {
+
+    @Inject
+    constructor() : this(
+            Executors.newSingleThreadExecutor(),
+            Executors.newFixedThreadPool(3),
+            MainThreadExecutor()
+    )
+
+    fun diskIO() = diskIO
+
+    fun networkIO() = networkIO
+
+    fun mainThread() = mainThread
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
