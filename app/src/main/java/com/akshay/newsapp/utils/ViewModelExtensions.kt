c542a20ce1f97eb5fe516f2c9c951222a7eb7fca
AkshayChordiya <akshaychordiya2@gmail.com>
2019-04-20 10:00:14 +0200

    Wire view model injection into activities

--- app/src/main/java/com/akshay/newsapp/utils/ViewModelExtensions.kt
+++ app/src/main/java/com/akshay/newsapp/utils/ViewModelExtensions.kt
@@ -4,12 +4,13 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.akshay.newsapp.ui.base.BaseActivity
 
 /**
  * Synthetic sugaring to get instance of [ViewModel] for [AppCompatActivity].
  */
-inline fun <reified T : ViewModel> AppCompatActivity.getViewModel(): T {
-    return ViewModelProviders.of(this).get(T::class.java)
+inline fun <reified T : ViewModel> BaseActivity.getViewModel(): T {
+    return ViewModelProviders.of(this, viewModelFactory).get(T::class.java)
 }
 
 /**
