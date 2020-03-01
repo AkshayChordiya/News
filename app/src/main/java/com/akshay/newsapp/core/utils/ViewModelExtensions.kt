3672940e0b6bdbea91c871a5481d7a47071fb3cc
AkshayChordiya <akshaychordiya2@gmail.com>
2020-03-01 17:27:59 +0100

    Replace deprecated ViewModelProviders

--- app/src/main/java/com/akshay/newsapp/core/utils/ViewModelExtensions.kt
+++ app/src/main/java/com/akshay/newsapp/core/utils/ViewModelExtensions.kt
@@ -3,19 +3,19 @@ package com.akshay.newsapp.core.utils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.akshay.newsapp.core.ui.base.BaseActivity
 
 /**
  * Synthetic sugaring to get instance of [ViewModel] for [AppCompatActivity].
  */
 inline fun <reified T : ViewModel> BaseActivity.getViewModel(): T {
-    return ViewModelProviders.of(this, viewModelFactory).get(T::class.java)
+    return ViewModelProvider(this, viewModelFactory).get(T::class.java)
 }
 
 /**
  * Synthetic sugaring to get instance of [ViewModel] for [Fragment].
  */
 inline fun <reified T : ViewModel> Fragment.getViewModel(): T {
-    return ViewModelProviders.of(this).get(T::class.java)
+    return ViewModelProvider(this).get(T::class.java)
 }
