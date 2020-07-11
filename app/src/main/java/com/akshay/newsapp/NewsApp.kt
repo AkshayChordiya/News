8e5c5b16cf4e449a625fcb78504311b4cadf02ad
patilshreyas <shreyaspatilg@gmail.com>
2020-07-11 12:12:38 +0530

    Setup Hilt in NewsApp

--- app/src/main/java/com/akshay/newsapp/NewsApp.kt
+++ app/src/main/java/com/akshay/newsapp/NewsApp.kt
@@ -1,28 +1,19 @@
 package com.akshay.newsapp
 
 import android.app.Application
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
-import javax.inject.Inject
 
-
-class NewsApp : Application(), HasAndroidInjector {
-
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+@HiltAndroidApp
+class NewsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        // Init DI magic ✨
-        AppInjector.init(this)
+
         // Plant timber
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
         }
     }
-
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
